718b629febf1794bc4af62df8b52d10a2af71f0a
p34-pac <peterpaultolulope@gmail.com>
2025-06-22 22:16:50 +0100

    update scrolltrigger

--- app/lib/useGsap.ts
+++ app/lib/useGsap.ts
@@ -1,7 +1,7 @@
 "use client"
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
 
 // if is client-side, register ScrollTrigger plugin
 if (typeof window !== "undefined") {
